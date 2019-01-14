8ea48f21f3e5165ee2b1ffbec92675c902da02a6
sdfnz <30536578+sdfnz@users.noreply.github.com>
2019-01-14 14:44:00 -0600

    Finished RGB page and did a little more cleaning up

--- hex_bg/hex.js
+++ hex_bg/hex.js
@@ -1,13 +1,10 @@
 const input = document.querySelector("input");
-document.body.style.backgroundColor = input.value;
+document.body.style.backgroundColor = '#' + input.value;
 
 input.addEventListener('input', changeBG);
 input.addEventListener('change', changeBG);
 
 function changeBG(e) {
 	let color = input.value;
-	if (color[0] != '#') {
-		color = '#' + color;
-	}
-	document.body.style.backgroundColor = color;
+	document.body.style.backgroundColor = '#' + color;
 };
--- rgb_bg/rgb.js
+++ rgb_bg/rgb.js
@@ -1,13 +1,21 @@
-const input = document.querySelector("#hex");
-document.body.style.backgroundColor = input.value;
+const red = document.querySelector("#red");
+const green = document.querySelector("#green");
+const blue = document.querySelector("#blue");
+let redVal = red.value;
+let greenVal = green.value;
+let blueVal = blue.value;
+document.body.style.backgroundColor = `rgb(${redVal},${greenVal},${blueVal})`;
 
-input.addEventListener('input', changeBG);
-input.addEventListener('change', changeBG);
+red.addEventListener('input', changeBG);
+red.addEventListener('change', changeBG);
+green.addEventListener('input', changeBG);
+green.addEventListener('change', changeBG);
+blue.addEventListener('input', changeBG);
+blue.addEventListener('change', changeBG);
 
 function changeBG(e) {
-	let color = input.value;
-	if (color[0] != '#') {
-		color = '#' + color;
-	}
-	document.body.style.backgroundColor = color;
+	let redVal = red.value;
+	let greenVal = green.value;
+	let blueVal = blue.value;
+	document.body.style.backgroundColor = `rgb(${redVal},${greenVal},${blueVal})`;
 };
