7b325015db450600583fdf46050b1a0f32338d4c
sdfnz <30536578+sdfnz@users.noreply.github.com>
2019-01-21 18:32:50 -0600

    Finished HSL page

---
+++ hsl_bg/hsl.js
@@ -0,0 +1,26 @@
+const hue = document.querySelector("#hue");
+const sat = document.querySelector("#sat");
+const light = document.querySelector("#light");
+let hueVal = hue.value;
+let hueOff = Number(hueVal) + 20;
+let satVal = sat.value;
+let lightVal = light.value;
+hue.style.color = `hsl(${String(hueOff)}, 50%, 50%)`;
+document.body.style.backgroundColor = `hsl(${hueVal}, ${satVal}%, ${lightVal}%)`;
+
+hue.addEventListener('input', changeBG);
+hue.addEventListener('change', changeBG);
+sat.addEventListener('input', changeBG);
+sat.addEventListener('change', changeBG);
+light.addEventListener('input', changeBG);
+light.addEventListener('change', changeBG);
+
+function changeBG(e) {
+	let hueVal = hue.value;
+	let hueOff = Number(hueVal) + 20;
+	console.log(hueOff);
+	let satVal = sat.value;
+	let lightVal = light.value;
+	hue.style.color = `hsl(${String(hueOff)}, 50%, 50%)`;
+	document.body.style.backgroundColor = `hsl(${hueVal}, ${satVal}%, ${lightVal}%)`;
+};
